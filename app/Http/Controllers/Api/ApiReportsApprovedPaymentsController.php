13a896a43fe7d474dc88d87e970050d0e717e107
Bosko Boskovicclear <bosko.boskovic@gmail.com>
2023-01-03 12:22:06 +0100

    Refactor method from payment service, new ApproverPaymentService

--- app/Http/Controllers/Api/ApiReportsApprovedPaymentsController.php
+++ app/Http/Controllers/Api/ApiReportsApprovedPaymentsController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Services\PaymentService;
+use App\Services\ApprovedPaymentService;
 
 class ApiReportsApprovedPaymentsController extends Controller
 {
+    protected ApprovedPaymentService $approverPaymentService;
+
+    public function __construct(ApprovedPaymentService $approverPaymentService)
+    {
+        $this->approverPaymentService = $approverPaymentService;
+    }
     /**
      * Sum of approved payments for every approver
      *
@@ -15,7 +21,7 @@ class ApiReportsApprovedPaymentsController extends Controller
     public function index()
     {
 
-        $sum_of_approved_payments = PaymentService::sumOfApprovedPaymentsPerUser();
+        $sum_of_approved_payments = $this->approverPaymentService->sum_of_approved_payments();
 
         return response()->json(
             [
--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -88,43 +88,4 @@ class PaymentService extends GlobalService
 
         return $this->success_response('Successfully deleted a payment', ['payment' => $cloned_payment]);
     }
-
-
-    /**
-     * Get sum of approved payments per user who is approver
-     *
-     * @return array
-     */
-    public static function sumOfApprovedPaymentsPerUser()
-    {
-        $approved_payments_sum = [];
-
-        $sum_of_approved_payments_query = DB::table('payments as p')
-            ->selectRaw('p.total_amount, p.id as payment_id')
-            ->join('payment_approvals as pa', 'p.id', '=', 'pa.payment_id')
-            ->join('users as u', 'pa.user_id', '=', 'u.id')
-            ->where('u.type', 'APPROVER')
-            ->where('pa.status', 'APPROVED')
-            ->groupBy('p.id')
-            ->havingRaw('COUNT(status) = (SELECT count(id) FROM users WHERE type = "APPROVER")')
-            ->get();
-
-        $collection = collect($sum_of_approved_payments_query);
-
-        $sum_of_approved_payments = $collection->sum('total_amount');
-
-        $payments_id = $collection->pluck('payment_id');
-
-        $payments = Payment::with('travel_payments')->whereIn('id', $payments_id)->get();
-
-        foreach (User::approvers() as $approver) {
-            $approved_payments_sum[] = [
-                'name' => $approver->full_name,
-                'sum_of_approved_payments' => (int) $sum_of_approved_payments,
-                'payments' => $payments,
-            ];
-        }
-
-        return $approved_payments_sum;
-    }
 }
